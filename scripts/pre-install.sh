49ea0eeced97c34e67cc80243d3d6da765971fd3
David Ghedini <david@davidghedini.com>
2020-04-10 18:07:17 +0300

    Update pre-install.sh

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -27,13 +27,14 @@ function install_webmin(){
 	elif [ "${REPO}" == 'rpm' ]; then
 
 cat >/etc/yum.repos.d/webmin.repo <<EOF
-		[Webmin]
-		name=Webmin Distribution Neutral
-		baseurl=http://download.webmin.com/download/yum
-		enabled=1
-		gpgcheck=1
-		gpgkey=http://www.webmin.com/jcameron-key.asc
+[Webmin]
+name=Webmin Distribution Neutral
+#baseurl=http://download.webmin.com/download/yum
+mirrorlist=http://download.webmin.com/download/yum/mirrorlist
+enabled=1
 EOF
+		wget http://www.webmin.com/jcameron-key.asc
+		rpm --import jcameron-key.asc
 		yum -y install webmin
 
 	fi
